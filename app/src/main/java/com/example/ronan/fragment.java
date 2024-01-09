9d9a4726c0a6d2a04d56849e108a72f8b9234135
user <hag.hispin12@gmail.com>
2024-01-09 16:29:34 +0200

    jhvb

--- app/src/main/java/com/example/ronan/fragment.java
+++ app/src/main/java/com/example/ronan/fragment.java
@@ -1,8 +1,14 @@
 package com.example.ronan;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +17,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,6 +35,25 @@ public class fragment extends Fragment {
     private ImageView img;
     private Button btn;
     ModelView vm1;
+    Uri uri;
+
+    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+
+
+    ActivityResultLauncher<Intent> startCamera = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                if(result.getResultCode()==RESULT_OK) {
+                    img.setImageURI(uri);
+                }
+                }
+            }
+    );
+
 
     @Nullable
 
@@ -43,6 +72,19 @@ public class fragment extends Fragment {
 
         Integer h = vm1.getRate();
         tv.setText(h+"");
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE,"New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION,"From Camera");
+                uri = requireContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+                startCamera.launch(cameraIntent);
+            }
+        });
 
     return view;
     }
