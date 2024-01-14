9b67b185600d4a01c6ffd0382fd3ccbb066d4316
user <hag.hispin12@gmail.com>
2024-01-14 15:17:10 +0200

    jhvb

---
+++ app/src/main/java/com/example/ronan/Fruit.java
@@ -0,0 +1,33 @@
+package com.example.ronan;
+
+import android.graphics.drawable.Drawable;
+
+public class Fruit extends user {
+    private String name;
+    private Drawable drawable;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public Fruit(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public Drawable getDrawable() {
+
+        return drawable ;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
