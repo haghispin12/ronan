7f5b6cedcc16cbf639890b7e13ee864aca343fcf
user <hag.hispin12@gmail.com>
2024-04-02 16:31:01 +0300

    jhvb

--- app/src/main/java/com/example/ronan/AFTMain.java
+++ app/src/main/java/com/example/ronan/AFTMain.java
@@ -1,5 +1,52 @@
 package com.example.ronan;
 
-public class AFTMain {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.Manifest;
+import android.widget.Toast;
+
+public class AFTMain extends AppCompatActivity {
+
+    private static int LOCATION_PERMISSION_REQUEST_CODE = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aftmain);
+
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+
+            requestLocationPermission();
+        }else{
+            //Location granted
+            //Do Something
+        }
+        }
+        private void requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Location is required for this app to function properly",Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_REQUEST_CODE);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Location granted
+                //Do something(Location updates)
+            } else {
+                //Location permission denied
+                //Handle the denial (show msg or disable location services)
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
 }
