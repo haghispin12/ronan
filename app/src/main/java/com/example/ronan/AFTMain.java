319b2e8f2afe224941265b31f8efa18eae347ca7
user <hag.hispin12@gmail.com>
2024-05-07 16:45:25 +0300

    jhvb

--- app/src/main/java/com/example/ronan/AFTMain.java
+++ app/src/main/java/com/example/ronan/AFTMain.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.Manifest;
 import android.widget.Toast;
 
-public class AFTMain extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
 
     private static int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private GoogleMap googleMap;
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+    private int requestCode;
+    private String[] permissions;
+    private int[] grantResults;
 
 
     @Override
@@ -20,20 +39,28 @@ public class AFTMain extends AppCompatActivity {
         setContentView(R.layout.activity_aftmain);
 
 
-        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             requestLocationPermission();
-        }else{
+        } else {
             //Location granted
             //Do Something
         }
-        }
-        private void requestLocationPermission(){
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-            Toast.makeText(this, "Location is required for this app to function properly",Toast.LENGTH_SHORT).show();
-        }
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_REQUEST_CODE);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+//        mapFragment.getMapAsync(this);
     }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Location is required for this app to function properly", Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -49,4 +76,35 @@ public class AFTMain extends AppCompatActivity {
         }
     }
 
-}
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    currentLocation = location;
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+                    mapFragment.getMapAsync(AFTMain.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(31.7683, 35.2137);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Jerusalem"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,12));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    }
+
