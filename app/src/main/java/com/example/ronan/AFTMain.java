cb8709a1f53f379d88e25b026c016a858bfbc80d
user <hag.hispin12@gmail.com>
2024-05-21 15:58:24 +0300

    LastLocation2

--- app/src/main/java/com/example/ronan/AFTMain.java
+++ app/src/main/java/com/example/ronan/AFTMain.java
@@ -12,7 +12,17 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.Manifest;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -25,6 +35,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 
 public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -33,13 +44,21 @@ public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
     private final int FINE_PERMISSION_CODE = 1;
     int n = 10;
     Location currentLocation;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
     private FusedLocationProviderClient fusedLocationClient;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aftmain);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        viewPager = findViewById(R.id.viewPager);
+//        tabLayout = findViewById(R.id.tabLayout);
+
 
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -53,24 +72,23 @@ public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
+
     @SuppressLint("MissingPermission")
     private void getLastLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-           return;
+            return;
         }
         fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-              @Override
-               public void onSuccess(Location location) {
-                 if (location != null) {
-                  currentLocation = location;
-                  setLocation(currentLocation.getLatitude(),currentLocation.getLongitude());
-//                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
-//                         mapFragment.getMapAsync(AFTMain.this);
-                     } else {
-                       Toast.makeText(AFTMain.this, "location not found", LENGTH_SHORT).show();
-                       }
-              }
-        }
+          @Override
+           public void onSuccess(Location location) {
+                if (location != null) {
+                currentLocation = location;
+                setLocation(currentLocation.getLatitude(), currentLocation.getLongitude());
+         } else {
+                 Toast.makeText(AFTMain.this, "location not found", LENGTH_SHORT).show();
+         }
+         }
+         }
         );
     }
 
@@ -78,21 +96,20 @@ public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
-            LatLng myLocation = new LatLng(32.79221, 35.53124);
-            googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
-            googleMap.getUiSettings().setZoomControlsEnabled(true);
-            googleMap.getUiSettings().isCompassEnabled();
-        }
+        LatLng myLocation = new LatLng(32.79221, 35.53124);
+        googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().isCompassEnabled();
+    }
 
-        public void setLocation(double latitude, double longitude){
-            LatLng myLocation = new LatLng(latitude, longitude);
-            googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
-            googleMap.getUiSettings().setZoomControlsEnabled(true);
-            googleMap.getUiSettings().isCompassEnabled();
-        }
-
+    public void setLocation(double latitude, double longitude) {
+        LatLng myLocation = new LatLng(latitude, longitude);
+        googleMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().isCompassEnabled();
+    }
 
     private void requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -100,9 +117,6 @@ public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
         }
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
     }
-
-
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -116,6 +130,5 @@ public class AFTMain extends AppCompatActivity implements OnMapReadyCallback {
         }
     }
 
-
-    }
+}
 
